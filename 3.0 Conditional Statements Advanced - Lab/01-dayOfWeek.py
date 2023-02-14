ee57e905dd3a7750499cdc57a2d138ac9bf0070e
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-02-15 00:11:59 +0200

    Update 01-dayOfWeek.py

--- 3.0 Conditional Statements Advanced - Lab/01-dayOfWeek.py
+++ 3.0 Conditional Statements Advanced - Lab/01-dayOfWeek.py
@@ -1,3 +1,16 @@
+import calendar
+
+try: print(calendar.day_name[int(input()) - 1])
+except: print('Error')
+
+# --------------------------------------------(2)----------------------
+lst = ["Error", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+try:
+    print(lst[int(input())])
+except:
+    print('Error')
+
+#------------------------------------------(3)--------------------------------
 def fun(arg):
     switcher = {
         1: "Monday",
@@ -16,3 +29,18 @@ print(fun(int(input())))
 
 # 7
 # output: Sunday
+    
+# ----------------------------------------(4)--------------------------- Warning not work in Judge in this time
+
+match int(input()) :
+    case 1: r = 'Monday'
+    case 2: r = 'Tuesday'
+    case 3: r = 'Wednesday'
+    case 4: r = 'Thursday'
+    case 5: r = 'Friday'
+    case 6: r = 'Saturday'
+    case 7: r = 'Sunday'
+    case other: r ='Error'
+print(r)
+
+    
