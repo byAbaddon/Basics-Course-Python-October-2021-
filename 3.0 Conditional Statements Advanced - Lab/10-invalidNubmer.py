02eba6fa1bd83fbc1f02db7a9bb32ee9cc9a4586
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-03-21 14:11:46 +0200

    Update 10-invalidNubmer.py

--- 3.0 Conditional Statements Advanced - Lab/10-invalidNubmer.py
+++ 3.0 Conditional Statements Advanced - Lab/10-invalidNubmer.py
@@ -1,3 +1,8 @@
+n = int(input())
+print('invalid' if not 100 <= n <= 200 and n != 0 else '')
+
+# -----------------------------------(2)-----------------------------
+
 n = float(input())
 print('invalid' if not (100 <= n and not n > 200 or n == 0) else '')
 
