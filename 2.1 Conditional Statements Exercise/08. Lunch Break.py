9f9655460fc7afc4e5a5d4c29c360069544746d1
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-04 16:42:33 +0300

    Update 08. Lunch Break.py

--- 2.1 Conditional Statements Exercise/08. Lunch Break.py
+++ 2.1 Conditional Statements Exercise/08. Lunch Break.py
@@ -1,16 +1,15 @@
 from math import ceil
 
-move, duration, breack = input(), int(input()), int(input())
+movie, duration, breack = input(), int(input()), int(input())
 
 lunch = breack / 8
 relax = breack / 4
 total_time = breack - lunch - relax
 
 if total_time >= duration:
-    print(f'You have enough time to watch {move} and left with {ceil(total_time - duration)} minutes free time.')
+    print(f'You have enough time to watch {movie} and left with {ceil(total_time - duration)} minutes free time.')
 else:
-    print(f"You don't have enough time to watch {move}, you need {ceil(duration - total_time)} more minutes.")
-
+    print(f"You don't have enough time to watch {movie}, you need {ceil(duration - total_time)} more minutes.")
     
 '''
 Game of Thrones
