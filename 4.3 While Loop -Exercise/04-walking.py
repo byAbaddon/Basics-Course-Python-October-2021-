23442b517cf0aed4f0b08d849f9aa8dbf7f9ea05
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-06 13:28:36 +0300

    Update 04-walking.py

--- 4.3 While Loop -Exercise/04-walking.py
+++ 4.3 While Loop -Exercise/04-walking.py
@@ -1,6 +1,4 @@
 steps = 10000
-steps_count = 0
-
 while steps > 0:
     el = input()
     if el != 'Going home':
@@ -10,9 +8,7 @@ while steps > 0:
         if steps > 0:
             print(f'{abs(steps)} more steps to reach goal.')
             exit()
-
 print(f'Goal reached! Good job!\n{abs(steps)} steps over the goal!')
-
 
 '''
 1000
